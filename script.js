bedb07a40f9ad6fc5184fe785cf6776a768f4468
Martalen <114415190+Martalen@users.noreply.github.com>
2024-04-09 13:22:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const br1Btn = document.getElementById("potatoBtn1")
+const br2Btn = document.getElementById("potatoBtn2")
+const br3Btn = document.getElementById("potatoBtn3")
+const br4Btn = document.getElementById("potatoBtn4")
+const br5Btn = document.getElementById("potatoBtn5")
+const br6Btn = document.getElementById("potatoBtn6")
+const br7Btn = document.getElementById("potatoBtn7")
+const br8Btn = document.getElementById("potatoBtn8")
+const br9Btn = document.getElementById("potatoBtn9")
+const brMenu = document.getElementById("brMenu")
+const brH = document.getElementById("brH")
+const brP = document.getElementById("brP")
+const brA = document.getElementById("brA")
+
+function roll(){
+    brMenu.display = block
+}
